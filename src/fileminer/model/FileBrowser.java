0884fddbde404e030f6e4eb0c64b46f6aa189b92
gambada <gambada@localhost>
2016-02-20 17:56:51 +0100

    Edited tree method and renamed in getTree

--- src/fileminer/model/FileBrowser.java
+++ src/fileminer/model/FileBrowser.java
@@ -2,30 +2,32 @@ package fileminer.model;
 
 import javax.swing.tree.DefaultMutableTreeNode;
 
-// Classe per la creazione del Tree del FileSystem
 /**
- * 
+ * Classe per la creazione del Tree del FileSystem
  * @author Daniele
  *
  */
 public interface FileBrowser {
-    
-    
-    /**
-     * @return null
-     */
-    DefaultMutableTreeNode setTree();
-    
-   
-    /**
-     * @param rootNode nodo root
-     */
-    void addGrandChilds(DefaultMutableTreeNode rootNode);
-    
-    /**
-     * @param rootNode nodo root
-     */
-    void addChilds(DefaultMutableTreeNode rootNode);
+
+
+	/**
+	 * Metodo per inserire il FileSystem nell'albero.
+	 * @return DefaultMutableTreeNode
+	 */
+	DefaultMutableTreeNode getTree();
+
+
+	/**
+	 * Aggiunge file alle sottocartelle all'albero.
+	 * @param rootNode root dell'albero
+	 */
+	void addGrandChilds(DefaultMutableTreeNode rootNode);
+
+	/**
+	 * Aggiunge i file della root all'albero.
+	 * @param rootNode root dell'albero
+	 */
+	void addChilds(DefaultMutableTreeNode rootNode);
 
 
 }
--- src/fileminer/model/FileBrowserImpl.java
+++ src/fileminer/model/FileBrowserImpl.java
@@ -2,93 +2,117 @@
 package fileminer.model;
 
 import java.io.File;
-import java.util.Collections;
 import java.util.Enumeration;
-import java.util.List;
 
 import javax.swing.filechooser.FileSystemView;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeNode;
 
 
-// Classe per la creazione del Tree del FileSystem
 /**
- * @author Daniele
- *
+ * Classe per la creazione del Tree del FileSystem.
  */
 public class FileBrowserImpl implements FileBrowser {
 
-    private FileSystemView fsv;
+	private FileSystemView fsv;
 
-    /**
-     * 
-     */
-    public FileBrowserImpl() {
-        this.fsv = FileSystemView.getFileSystemView();
-    }
-
-
-    /**
-     * @return DefaultMutableTreeNode
-     */
-    public DefaultMutableTreeNode setTree() {
-
-        // Creo il nodo root
-        DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode();
-
-        // Assegno la root
-        for (File file : fsv.getRoots()) {
-            rootNode.add(new DefaultMutableTreeNode(new NodeImpl(file)));
-        }
-
-        addChilds(rootNode);
-        addGrandChilds(rootNode);
-
-        return new DefaultMutableTreeNode(rootNode); 
-    }
+	/**
+	 * Costruttore di FileBrowserImpl inizializza la FileSystemView.
+	 */
+	public FileBrowserImpl() {
+		this.fsv = FileSystemView.getFileSystemView();
+	}
 
 
-    @Override
-    public void addGrandChilds(final DefaultMutableTreeNode rootNode) {
-        Enumeration<?> enumeration = rootNode.children();
-        
-        // Aggiungo figli dei figli ai vari nodi se presenti
-        while (enumeration.hasMoreElements()) {
-            DefaultMutableTreeNode node = (DefaultMutableTreeNode) enumeration.nextElement();
-            addChilds(node);
-        }
-    }        
+	@Override
+	public DefaultMutableTreeNode getTree() {
+
+		// Creo il nodo root
+		DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode();
+
+		// Ottengo le roots
+		File[] roots = fsv.getRoots();
+
+		for (File fsRoot : roots) {
+			DefaultMutableTreeNode node = new DefaultMutableTreeNode(fsRoot);
+			rootNode.add(node);
+			File[] files = fsv.getFiles(fsRoot, true);
+			for (File file : files) {
+				if (file.isDirectory()) {
+					node.add(new DefaultMutableTreeNode(file));
+				}
+			}
+		}
+
+		addChilds(rootNode);
+		addGrandChilds(rootNode);
+
+		//Test di funzionamento
+		printTree(rootNode);
+
+		return new DefaultMutableTreeNode(rootNode); 
+	}
 
 
-    @Override
-    public void addChilds(final DefaultMutableTreeNode rootNode) {
-        Enumeration<?> enumeration = rootNode.children();
-        
-        // Aggiungo figli ai vari nodi
-        while (enumeration.hasMoreElements()) {
-            DefaultMutableTreeNode node = (DefaultMutableTreeNode) enumeration.nextElement();
-            NodeImpl fileNode = (NodeImpl) node.getUserObject();
-            File file = fileNode.getFile();
-            // Controllo se è una directory
-            if (file.isDirectory() && !isEmpty(file.listFiles())) {
-                // Aggiungo i file nella directory se presenti
-                for (File child : file.listFiles()) {
-                    node.add(new DefaultMutableTreeNode(new NodeImpl(child)));
-                }
-            }
-        }        
-    }
+	@Override
+	public void addGrandChilds(final DefaultMutableTreeNode rootNode) {
+		Enumeration<?> enumeration = rootNode.children();
 
-    // Controlla se la lista files è vuota
-    private static boolean isEmpty(final File[] files) {
-        return files.length <= 0;
-    }
-    
-    /**
-     * @return FileSystemView
-     */
-    public FileSystemView getView() {
-        return fsv;
-    }
+		// Aggiungo figli dei figli ai vari nodi se presenti
+		while (enumeration.hasMoreElements()) {
+			DefaultMutableTreeNode node = (DefaultMutableTreeNode) enumeration.nextElement();
+			addChilds(node);
+		}
+	}        
+
+
+	@Override
+	public void addChilds(final DefaultMutableTreeNode rootNode) throws NullPointerException {		
+		Enumeration<?> enumeration = rootNode.children();
+		while (enumeration.hasMoreElements()) {
+			DefaultMutableTreeNode node = 
+					(DefaultMutableTreeNode) enumeration.nextElement();
+			File file = (File) node.getUserObject();
+			if (file.isDirectory() && !isEmpty(file.listFiles())) {
+				for (File child : file.listFiles()) {
+					node.add(new DefaultMutableTreeNode(
+							child));
+				}
+			}
+		}
+	}
+
+	/**
+	 * Controlla se la lista files è vuota
+	 * @param files
+	 * @return 
+	 * 		false: File[] is empty
+	 * 		true: File[] is not empty
+	 */
+	private static boolean isEmpty(final File[] files) {
+		return files.length <= 0;
+	}
+
+	/**
+	 * @return FileSystemView
+	 */
+	public FileSystemView getView() {
+		return fsv;
+	}
+
+
+	/**
+	 * Stampa l'albero del FileSystem
+	 */
+	private static void printTree(final DefaultMutableTreeNode root) {
+		@SuppressWarnings("unchecked")
+		Enumeration<DefaultMutableTreeNode> en = root.preorderEnumeration();
+		while (en.hasMoreElements()) {
+			DefaultMutableTreeNode node = en.nextElement();
+			TreeNode[] path = node.getPath();
+			System.out.println((node.isLeaf() ? "  - " : "+ ") + path[path.length - 1]);
+		}
+	}
 
 }
 
