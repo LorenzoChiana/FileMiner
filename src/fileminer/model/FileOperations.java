d1b0dc8d15301647bf3f5a2b63d83241335bf1ad
gambada <gambada@localhost>
2016-02-20 18:37:19 +0100

    Added operations to FileOperations

--- src/fileminer/model/FileOperations.java
+++ src/fileminer/model/FileOperations.java
@@ -1,5 +1,8 @@
 package fileminer.model;
 
+import java.io.File;
+import java.nio.file.Path;
+
 // Interfaccia per le varie operazioni sui files
 /**
  * @author Daniele
@@ -7,4 +10,48 @@ package fileminer.model;
  */
 public interface FileOperations {
 
+	/**
+	 * @param src Percorso file sorgente
+	 */
+	void copy(File src);
+	
+
+	/**
+	 * @param path Percorso dove incollare
+	 */
+	void pasteTo(String path);
+	
+	/**
+	 * 
+	 */
+	void cut();
+	
+	/**
+	 * @param path Percorso dove spostare
+	 */
+	void moveTo(String path);
+	
+	/**
+	 * 
+	 */
+	void open();
+	
+	/**
+	 * 
+	 */
+	void remove();
+	
+	/**
+	 * 
+	 */
+	void print();
+	
+	/**
+	 * @return file
+	 */
+	File getFileSrc();
+	
+	
+	
+	
 }
--- src/fileminer/model/FileOperationsImpl.java
+++ src/fileminer/model/FileOperationsImpl.java
@@ -1,10 +1,64 @@
 package fileminer.model;
 
+import java.io.File;
+
 // Classe per le varie operazioni sui files
 /**
  * @author Daniele
  *
  */
 public class FileOperationsImpl implements FileOperations {
+	
+	private File src;
+
+	@Override
+	public void copy(final File src) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pasteTo(final String path) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cut() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveTo(final String path) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void open() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public File getFileSrc() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 
 }
