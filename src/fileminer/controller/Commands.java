5f2e8329f97e14558629b2b4e58207c5fb35f244
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2016-02-20 16:16:45 +0100

    Implemented a draft for basic operations

---
+++ src/fileminer/controller/Commands.java
@@ -0,0 +1,17 @@
+package fileminer.controller;
+
+/**
+ * 
+ * @author Lorenzo Chiana
+ *      enum with file system's basic operations.
+ */
+public enum Commands {
+    COPY,
+    PASTE,
+    CUT,
+    MOVE,
+    LINK,
+    DELETE,
+    NEW,
+    MODIFY;
+}
--- src/fileminer/controller/ControllerImpl.java
+++ src/fileminer/controller/ControllerImpl.java
@@ -22,13 +22,38 @@ public class ControllerImpl implements Controller {
         
         //faccio partire la gui
         gui.start();
+    }
 
-        /*SwingUtilities.invokeLater(new Runnable() {
+    @Override
+    public void invokesCommand(final String command) {
+        
+        SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-
+                /*
+                 * uso la programmazione concorrente per avviare le operazioni di
+                 * copia, incolla, taglia, sposta, cancella, link, crea... ecc ecc
+                 */
+                
+                if (Commands.COPY.toString().equals(command)) {
+                    /*copy*/
+                } else if (Commands.PASTE.toString().equals(command)) {
+                    /*paste*/
+                } else if (Commands.CUT.toString().equals(command)) {
+                    /*cut*/
+                } else if (Commands.MOVE.toString().equals(command)) {
+                    /*move*/
+                } else if (Commands.LINK.toString().equals(command)) {
+                    /*link*/
+                } else if (Commands.DELETE.toString().equals(command)) {
+                    /*delete*/
+                } else if (Commands.NEW.toString().equals(command)) {
+                    /*new*/
+                } else if (Commands.MODIFY.toString().equals(command)) {
+                    /*modify*/
+                }
             }
-        });*/
+        });
     }
 
 }
