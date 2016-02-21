c3b4d1f9e3c6a3955dcd5a7860b6f0a64b1d5558
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2016-02-21 13:52:49 +0100

    ControllerImpl edited

--- src/fileminer/controller/ControllerImpl.java
+++ src/fileminer/controller/ControllerImpl.java
@@ -1,9 +1,13 @@
 package fileminer.controller;
 
+import java.io.File;
+
 import javax.swing.SwingUtilities;
 
 import fileminer.model.FileBrowser;
 import fileminer.model.FileBrowserImpl;
+import fileminer.model.FileOperations;
+import fileminer.model.FileOperationsImpl;
 import fileminer.view.FileMinerGUI;
 
 /**
@@ -27,6 +31,8 @@ public class ControllerImpl implements Controller {
     @Override
     public void invokesCommand(final String command) {
         
+        final FileOperations model = new FileOperationsImpl();
+        
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -36,17 +42,17 @@ public class ControllerImpl implements Controller {
                  */
                 
                 if (Commands.COPY.toString().equals(command)) {
-                    /*copy*/
+                    model.copy(model.getFileSrc()); /*cambiare new file con effettivo file*/
                 } else if (Commands.PASTE.toString().equals(command)) {
-                    /*paste*/
+                    model.pasteTo("");
                 } else if (Commands.CUT.toString().equals(command)) {
-                    /*cut*/
+                    model.cut();
                 } else if (Commands.MOVE.toString().equals(command)) {
-                    /*move*/
+                    model.moveTo("");
                 } else if (Commands.LINK.toString().equals(command)) {
                     /*link*/
                 } else if (Commands.DELETE.toString().equals(command)) {
-                    /*delete*/
+                    model.remove();
                 } else if (Commands.NEW.toString().equals(command)) {
                     /*new*/
                 } else if (Commands.MODIFY.toString().equals(command)) {
