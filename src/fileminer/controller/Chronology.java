7f8f7fa431639205b7554c22afdc2c4c20be5748
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2016-02-23 15:48:28 +0100

    Modified chronology

--- src/fileminer/controller/Chronology.java
+++ src/fileminer/controller/Chronology.java
@@ -21,4 +21,17 @@ public interface Chronology {
      *          This list represents the directory chronology.
      */
     List<String> getChronology();
+    /**
+     * Go forward in the list of directory. 
+     */
+    void nextDir();
+    /**
+     * Go backward in the list of directory.
+     */
+    void prevDir();
+    /**
+     * 
+     * @return the path of the current directory.
+     */
+    String getCurrentDirectory();
 }
--- src/fileminer/controller/ChronologyImpl.java
+++ src/fileminer/controller/ChronologyImpl.java
@@ -10,17 +10,36 @@ import java.util.List;
  */
 public class ChronologyImpl implements Chronology {
     private List<String> chronology = new ArrayList<>();
-    /*
-     * da implementare meglio.
-     */
+    private Integer indexCurrentDir;
+    
     @Override
     public void addDirectory(final String path) {
         this.chronology.add(path);
+        this.indexCurrentDir = this.chronology.size();
     }
 
     @Override
     public List<String> getChronology() {
         return this.chronology;
+    }
+
+    @Override
+    public void nextDir() {
+        if (this.indexCurrentDir < this.chronology.size()) {
+            this.indexCurrentDir++;            
+        }
+    }
+
+    @Override
+    public void prevDir() {
+        if (this.indexCurrentDir == 0) {
+            this.indexCurrentDir--;
+        }
+    }
+
+    @Override
+    public String getCurrentDirectory() {
+        return this.chronology.get(this.indexCurrentDir);
     }
 
 }
