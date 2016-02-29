910e1f737f1108cf855a950f9c3c9b4a2fc6024e
gambada <gambada@localhost>
2016-02-29 16:28:29 +0100

    Edited a bit the Test class

--- src/fileminer/model/test/Test.java
+++ src/fileminer/model/test/Test.java
@@ -1,29 +1,35 @@
 package fileminer.model.test;
 
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreePath;
 
 import fileminer.model.FileOperationsImpl;
 import fileminer.model.FileSystemTreeImpl;
+import fileminer.model.Node;
 
 
 public class Test {
-	
+
 	private static final String NAME = "FileMinerApp";
-	
+
 	@org.junit.Test
-	public void testOK() {
+	public void test() {
 		FileSystemTreeImpl fsti = new FileSystemTreeImpl();
 		DefaultTreeModel model = fsti.getTree();
 		DefaultMutableTreeNode tree = new DefaultMutableTreeNode(model); 
 		FileOperationsImpl foi = new FileOperationsImpl(fsti);
-		String dir = System.getProperty("user.home")+ System.getProperty("file.separator") + NAME;
+		final Path path = FileSystems.getDefault().getPath(System.getProperty("user.home"));
+		tree.add(new DefaultMutableTreeNode(new File(path.toUri())));
+		TreePath tp = new TreePath(tree.getLastLeaf().getUserObjectPath());
 		
-		
-       
-		
-		
-		
+
+
 	}
 	//Crea cartella desktop
 	//Crea file
