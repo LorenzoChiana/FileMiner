24c10c50c8b85fae68549e8ddede3ee4bf322a6c
gambada <gambada@localhost>
2016-02-29 13:12:29 +0100

    Edited Test class

--- src/fileminer/model/test/Test.java
+++ src/fileminer/model/test/Test.java
@@ -1,8 +1,34 @@
 package fileminer.model.test;
 
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+
+import fileminer.model.FileOperationsImpl;
+import fileminer.model.FileSystemTreeImpl;
+
+
 public class Test {
-	//Copia
-	//incolla
+	
+	private static final String NAME = "FileMinerApp";
+	
+	@org.junit.Test
+	public void testOK() {
+		FileSystemTreeImpl fsti = new FileSystemTreeImpl();
+		DefaultTreeModel model = fsti.getTree();
+		DefaultMutableTreeNode tree = new DefaultMutableTreeNode(model); 
+		FileOperationsImpl foi = new FileOperationsImpl(fsti);
+		String dir = System.getProperty("user.home")+ System.getProperty("file.separator") + NAME;
+		
+		
+       
+		
+		
+		
+	}
+	//Crea cartella desktop
+	//Crea file
+	//Copia cartella desktop
+	//incolla nel desktop
 	//Comprimi
 	//Decomprimi
 	//Rimuovi tutto
