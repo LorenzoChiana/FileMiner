8fa893afbf505020a938d6c151179be5741d6c08
gambada <gambada@localhost>
2016-03-01 21:22:59 +0100

    Corrected ArchiveZIP class

--- src/fileminer/model/archiver/Archiver.java
+++ src/fileminer/model/archiver/Archiver.java
@@ -1,6 +1,7 @@
 package fileminer.model.archiver;
 
 import java.io.FileNotFoundException;
+import java.nio.file.FileAlreadyExistsException;
 import java.util.List;
 
 import javax.swing.tree.TreePath;
@@ -21,6 +22,7 @@ public interface Archiver {
      * @param dest destination of archive
      * @throws FileNotFoundException file not found
      * @throws ZipException zip exception
+     * @throws FileAlreadyExistsException 
      */
     void compress(List<TreePath> paths,  String name,  TreePath dest) throws FileNotFoundException, ZipException;
 
@@ -32,6 +34,7 @@ public interface Archiver {
      * @param dest destination of decompressed files
      * @throws FileNotFoundException file not found
      * @throws ZipException zip exception
+     * @throws FileAlreadyExistsException 
      */
     void decompress(List<TreePath> archives, TreePath dest) throws FileNotFoundException, ZipException;
 
